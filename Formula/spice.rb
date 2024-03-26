5f6d2d4fc5b6761afb9871fb1845c0a690595bce
yfu <fevin86@gmail.com>
2024-03-26 12:40:29 +1100

    update to 0.9.1

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -5,22 +5,22 @@ class Spice < Formula
   desc "Spice.ai CLI"
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.9-alpha"
+  version "v0.9.1-alpha"
   revision 1
 
   if OS.mac?
     if Hardware::CPU.arm?
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_darwin_aarch64.tar.gz"
-        sha256 "2ba8a309b80dba160f56f938962bca0014e8414fe9e635a600175b155cf2004a"
+        sha256 "48ba81df256a543ed28e5c7ac744b5164388adac40dcf34e58b9ea5d6296cbe5"
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_darwin_x86_64.tar.gz"
-        sha256 "28227e7f02be240a5d96c609a472b6960cd67e7136861a0096bd42df86346ffd"
+        sha256 "87162c66857adfb4aad6cd1a60b0d217e83080cc95f6d9522440cef2c17aa961"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_linux_x86_64.tar.gz"
-        sha256 "eaf89b7ef221837091bd8a6a435401fec12c4d249bc0474cceefeac9c64630e2"
+        sha256 "7c0ebd69765d105ddd04c90f0be987f796e4d4a1c90c14ef0ee1ec6ad8d6ebce"
     end
   end
 
