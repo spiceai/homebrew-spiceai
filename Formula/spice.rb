0676e2a8c60152d3ea19fcf5e469e2424469194d
yfu <fevin86@gmail.com>
2024-03-28 12:53:33 +1100

    upgrade to 0.10 (#3)
    * update version to v0.10.0-alpha
    
    * readme

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -1,26 +1,29 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
+$darwin_aarch64_sha256 = "560e7fae87beb731e42a639837e9f928ba89a9dc410daf6324af8496c730976d"
+$darwin_x86_64_sha256 = "c42592d4e12e1d05c412ceada3a4a3ddeff96872bf77f29ef44c61374bf91882"
+$linux_x86_64_sha256 = "3ec9c1e05fa0900da05d2d4ed556e61d0f248177fde5992dc24cc612c18ffc51"
 
 class Spice < Formula
   desc "Spice.ai CLI"
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.9.1-alpha"
+  version "v0.10.0-alpha"
   revision 1
 
   if OS.mac?
     if Hardware::CPU.arm?
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_darwin_aarch64.tar.gz"
-        sha256 "48ba81df256a543ed28e5c7ac744b5164388adac40dcf34e58b9ea5d6296cbe5"
+        sha256 $darwin_aarch64_sha256
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_darwin_x86_64.tar.gz"
-        sha256 "87162c66857adfb4aad6cd1a60b0d217e83080cc95f6d9522440cef2c17aa961"
+        sha256 $darwin_x86_64_sha256
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spice_linux_x86_64.tar.gz"
-        sha256 "7c0ebd69765d105ddd04c90f0be987f796e4d4a1c90c14ef0ee1ec6ad8d6ebce"
+        sha256 $linux_x86_64_sha256
     end
   end
 
--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,26 +1,29 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
+$darwin_aarch64_sha256 = "0cbc6379efdbcbfa27374ab208db8697503040de9085eef4f2ae0abeee18fafa"
+$darwin_x86_64_sha256 = "aa653c43ac069ef506fae88e5aa9862abbeb70e88c67e4d094902af3b543db48"
+$linux_x86_64_sha256 = "f4abb980735026b6bb613d5062b3513238b15e7c4ec9beca55c194bdb93e2b93"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.9.1-alpha"
+  version "v0.10.0-alpha"
   revision 1
 
   if OS.mac?
     if Hardware::CPU.arm?
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_darwin_aarch64.tar.gz"
-        sha256 "b993d0375863b1f73ead1288029e0723bdf2852c1b384a818ac04f3f1d4cf7de"
+        sha256 $darwin_aarch64_sha256
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_darwin_x86_64.tar.gz"
-        sha256 "53e5db505799bcb85295c886e8b2d1b250a24fa15228b7d69409b75fcc3b0e8d"
+        sha256 $darwin_x86_64_sha256
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
     else
         url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_linux_x86_64.tar.gz"
-        sha256 "6ca57857f37d4309257fcb392141341989736548589daee04f5b34ddc8cb55cd"
+        sha256 $linux_x86_64_sha256
     end
   end
 
