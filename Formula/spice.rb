a06ef9e319d6b7b067d8a5e080d927d96f5564cb
Phillip LeBlanc <phillip@leblanc.tech>
2024-05-07 11:39:03 +0900

    homebrew versions for v0.12.1-alpha

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "3d67250e3f8ea4074205c13b860977bc3b7eb7b7ef49339ba4381214b0721e74"
-$darwin_x86_64_sha256 = "f6e39ef3c8443c2830091f39ed13c9ea937c7c326959e062722cc1bb69bb78e0"
-$linux_x86_64_sha256 = "5f9badde9ebc721a6d4d50319e0d0ddcf80a02a21e3352f0e221c3ede6656374"
+$darwin_aarch64_sha256 = "ec1b588f51b29bc6d415892cba4c53b341150bfb2773d4376e1045a5c5e8d04f"
+$darwin_x86_64_sha256 = "f14615d5d47b6da6719d19dc95bd5b5f486bcb43d7a38d511207b2f18d8af60f"
+$linux_x86_64_sha256 = "bd11c8ad48f66b18844a8da817971fe0a2ea1c9b9fa6728c455ae608cf3eb7da"
 
 class Spice < Formula
   desc "Spice.ai CLI"
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.12.0-alpha"
+  version "v0.12.1-alpha"
   revision 1
 
   if OS.mac?
--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "3b0541d9fc77266641e24b533c881adadbf337e0748ad1c27cb6c872169fe41d"
-$darwin_x86_64_sha256 = "1571ae19bed74c0c8183e7a1d5c5286cf0e25cd5abac374693837a7b5ee1a258"
-$linux_x86_64_sha256 = "f1d9ff848ad89e4cd98eb711be81e5d276d075772d17f3e54194a6f209652a8d"
+$darwin_aarch64_sha256 = "486be731aca218ab424389cd8d2aae6af631f6d52f3839d956d6167055050554"
+$darwin_x86_64_sha256 = "254ceda32f06bd3ce63fb6532a591a18b6f2a5b939a99d4d49f439bfedcf7e30"
+$linux_x86_64_sha256 = "acb0c9b8c0ad9240a21c352ef74e31cc6090ed376021580bfedb53d0c58e83ed"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.12.0-alpha"
+  version "v0.12.1-alpha"
   revision 1
 
   if OS.mac?
