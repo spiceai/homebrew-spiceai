202e98f4783617c41da9e37a17225996bc00b2b5
Sergei Grebnov <sergei.grebnov@gmail.com>
2024-05-13 18:52:12 -0700

    homebrew versions for v0.12.2-alpha (#11)

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "ec1b588f51b29bc6d415892cba4c53b341150bfb2773d4376e1045a5c5e8d04f"
-$darwin_x86_64_sha256 = "f14615d5d47b6da6719d19dc95bd5b5f486bcb43d7a38d511207b2f18d8af60f"
-$linux_x86_64_sha256 = "bd11c8ad48f66b18844a8da817971fe0a2ea1c9b9fa6728c455ae608cf3eb7da"
+$darwin_aarch64_sha256 = "37153205c928236a9fb50ab9325dea5f8bdf82cbc6a80515d927acb28d1e21df"
+$darwin_x86_64_sha256 = "7e22310f550140380dc036568a3be0af3188142577beb1a89c443457a16e13f8"
+$linux_x86_64_sha256 = "89522dcc929509a9c4c05bcce21ac9b0a96934b748553827334f05631d6fc317"
 
 class Spice < Formula
   desc "Spice.ai CLI"
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.12.1-alpha"
+  version "v0.12.2-alpha"
   revision 1
 
   if OS.mac?
