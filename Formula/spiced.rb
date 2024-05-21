743cf994eb461432e16a81524dc77c71a1b86364
Jack Eadie <jack.eadie0@gmail.com>
2024-05-21 21:15:40 +1000

    update spiced.rb for v0.13.0-alpha

--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,8 +1,8 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "cd3183e291bda402c0e8c1f32ac953e71392d0784c86b314f39d641c1c79ee4d"
-$darwin_x86_64_sha256 = "8398f4134d74750f080235a1abf134ad4c2d8f27af2c1767721d142fcc78e8a2"
-$linux_x86_64_sha256 = "23bd078e6f695d343f7ce120a23288039755f671f517e2fca4f7574ccb3004f5"
+$darwin_aarch64_sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+$darwin_x86_64_sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+$linux_x86_64_sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
