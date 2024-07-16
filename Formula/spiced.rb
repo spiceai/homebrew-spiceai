e13ac5229cf08a39d87c343d8d6eba692cd18b4b
Evgenii Khramkov <evgenii@spice.ai>
2024-07-16 10:55:27 +0900

    update homebrew taps for v0.15.2-alpha (#20)

--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "dc7a457e924aee7a56eb576207c0f8e3ce9d5834a14ad6d500bfa9865415356e"
-$darwin_x86_64_sha256 = "369d8379fc711863c41a9d324083db3ae0b64f4ea4065f732b84bfe834340024"
-$linux_x86_64_sha256 = "74a0374f99274c896e1357614d430b32caabef1e09f807e95cd275fe60e98e37"
+$darwin_aarch64_sha256 = "8839bde8c84485b293d190d87a68af7a4674c605381142515c1f3deadbf4d2e8"
+$darwin_x86_64_sha256 = "d7a97b4169867e10927b9d037f469366e1b49cf3073ddf290173a679f0defb9f"
+$linux_x86_64_sha256 = "702eb7cc8bdf6b69f77ff67335b2fa832c20c58021fa03e3a7caf43262bea7b9"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.15.1-alpha"
+  version "v0.15.2-alpha"
   revision 1
 
   if OS.mac?
