69be3676fc2e4305a7f348f4868ea560deebc69c
yfu <fevin86@gmail.com>
2024-07-30 10:46:13 +1000

    update to 0.17 beta

--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "3d5e2b84a99d4b496b28ce696573cbb19ec7f0edb7b64d64c2c29e0f0428f441"
-$darwin_x86_64_sha256 = "90e3e941178174781d7fb7f026f2a90aa2fb4bdfff0ecfc35fb9f3d45e8ee6f4"
-$linux_x86_64_sha256 = "b7ab3e4c8fc57dc383a4ac7882dd94d59048d575d87cbf81407be7106a47f534"
+$darwin_aarch64_sha256 = "d9ab7e75a13de3d511c9f34b9dc6477a10d3f220899536cd8886755e36e86e40"
+$darwin_x86_64_sha256 = "eb65516f1b49e34b781069a5e1674f7ad5abfa928fd97b7ddeb544bc8e483068"
+$linux_x86_64_sha256 = "da70ce7c8c0b623747c11286704845889740781f7ea9367e9ab9be271388afb7"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.16.0-alpha"
+  version "v0.17.0-beta"
   revision 1
 
   if OS.mac?
