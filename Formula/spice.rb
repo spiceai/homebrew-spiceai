16391d4420742e53a93b62df904a75459142dd4d
peasee <98815791+peasee@users.noreply.github.com>
2024-09-10 13:00:25 +1000

    release: v0.17.4-beta

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -1,14 +1,14 @@
 $pkg     = "github.com/spiceai/spiceai"
 $tags    = %w()
-$darwin_aarch64_sha256 = "9c9d0d8462f8b9186077895985b152b6a3edd65d8f47eb7a28c82069c642946f"
-$darwin_x86_64_sha256 = "aeac5c63109788bae6d0e102a4891595c9face558a39c45853bc627abe149969"
-$linux_x86_64_sha256 = "793374fa42640b9272de1fe67f55f817e28a8a7a369f7e536b063c917efce525"
+$darwin_aarch64_sha256 = "2160f240946ef7cd82f2a388aa0593718f6024dd76813e58cca3791503ba2eb9"
+$darwin_x86_64_sha256 = "860b265c76d2a983130683c8b4f2586e4010a667ed74e6a017eee5a925d83c94"
+$linux_x86_64_sha256 = "3b8bc88a9118467a4329f97aaedd8655b00ff55b9712f2386bd3eb23ff15e957"
 
 class Spice < Formula
   desc "Spice.ai CLI"
   homepage "https://#{$pkg}"
   url "https://#{$pkg}"
-  version "v0.17.3-beta"
+  version "v0.17.4-beta"
   revision 1
 
   if OS.mac?
