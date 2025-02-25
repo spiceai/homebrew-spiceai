7aaa47414585a995ddae1d8538ad65f034295069
Garam Choi <Starever222@gmail.com>
2025-02-25 11:39:49 +0900

    Serve all supported OS architectures & Shows docs link if user OS arch is unsupported (#55)
    * Serve all supported OS archs and shows docs link if unsupported
    * Add omitted linux aarch64
    * return error message for unsupported os arch
    
    * Align error message with script installation
    
    * Show exact os arch of user in error msg

--- Formula/spiced.rb
+++ Formula/spiced.rb
@@ -1,8 +1,7 @@
-$pkg     = "github.com/spiceai/spiceai"
-$tags    = %w()
+$pkg = "github.com/spiceai/spiceai"
 $darwin_aarch64_sha256 = "5c31f6c1ad7c476ec5718b73d7a0f5786e980df1893092360004c550c4322502"
-$darwin_x86_64_sha256 = "acd2268c5df56ee7a601bbc4c5670d34a6c8dea3731f17183351458d56c3575b"
 $linux_x86_64_sha256 = "91d8af95e456141886b9277bc12b6154ab6a66408803487a2f8ea5a60922f37a"
+$linux_aarch64_sha256 = "78a0ff86e796c42124ad5bc3cc3fca63efc3fa6a77fe74da9d62f09d91c7003e"
 
 class Spiced < Formula
   desc "A unified SQL query interface and portable runtime to locally materialize, accelerate, and query data tables sourced from any database, data warehouse, or data lake."
@@ -11,19 +10,38 @@ class Spiced < Formula
   version "v1.0.4"
   revision 1
 
-  if OS.mac?
+  os = `uname`.strip.downcase
+  arch = `uname -m`.strip
+
+  case arch
+  when /armv7.*/ then arch = "arm"
+  when "arm64" then arch = "aarch64"
+  when "amd64" then arch = "x86_64"
+  end
+
+  current_osarch = "#{os}-#{arch}"
+
+  BASE_URL = "https://#{$pkg}/releases/download/#{version}"
+  ERROR_MSG = "#{current_osarch} does not have a pre-built binary. For supported architectures, visit https://spiceai.org/docs/reference/system_requirements#operating-systems-and-architectures"
+
+  on_macos do
     if Hardware::CPU.arm?
-        url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_darwin_aarch64.tar.gz"
-        sha256 $darwin_aarch64_sha256
+      url "#{BASE_URL}/spice_darwin_aarch64.tar.gz"
+      sha256 $darwin_aarch64_sha256
     else
-        url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_darwin_x86_64.tar.gz"
-        sha256 $darwin_x86_64_sha256
+      odie ERROR_MSG
     end
-  elsif OS.linux?
+  end
+
+  on_linux do
     if Hardware::CPU.arm?
+      url "#{BASE_URL}/spice_linux_aarch64.tar.gz"
+      sha256 $linux_aarch64_sha256
+    elsif Hardware::CPU.intel?
+      url "#{BASE_URL}/spice_linux_x86_64.tar.gz"
+      sha256 $linux_x86_64_sha256
     else
-        url "https://github.com/spiceai/spiceai/releases/download/#{version}/spiced_linux_x86_64.tar.gz"
-        sha256 $linux_x86_64_sha256
+      odie ERROR_MSG
     end
   end
 
